e942bfa24fba70cbe11b94c6a9ce6e4108901afd
Vorobey <vorobyovvorobyov@gmail.com>
2023-11-22 21:22:28 +0300

    Add tests

---
+++ Assets/UGF.Assets.Editor.Tests/TestAssetCollectionAsset.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace UGF.Assets.Editor.Tests
+{
+    [CreateAssetMenu(menuName = "Tests/TestAssetCollectionAsset")]
+    public class TestAssetCollectionAsset : ScriptableObject
+    {
+        [SerializeField] private List<Object> m_assets = new List<Object>();
+
+        public List<Object> Assets { get { return m_assets; } }
+    }
+}
---
+++ Assets/UGF.Assets.Editor.Tests/TestAssetCollectionFolderAsset.cs
@@ -0,0 +1,28 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace UGF.Assets.Editor.Tests
+{
+    [CreateAssetMenu(menuName = "Tests/TestAssetCollectionFolderAsset")]
+    public class TestAssetCollectionFolderAsset : AssetFolderAsset<TestAssetCollectionAsset, Object>
+    {
+        protected override void OnUpdate()
+        {
+            Collection.Assets.Clear();
+
+            string[] guids = FindAssetsAsGuids();
+
+            for (int i = 0; i < guids.Length; i++)
+            {
+                string guid = guids[i];
+                string path = AssetDatabase.GUIDToAssetPath(guid);
+
+                var asset = AssetDatabase.LoadAssetAtPath<Object>(path);
+
+                Collection.Assets.Add(asset);
+            }
+
+            EditorUtility.SetDirty(Collection);
+        }
+    }
+}
