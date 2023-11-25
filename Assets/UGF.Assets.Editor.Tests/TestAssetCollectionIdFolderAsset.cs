f2708fdd9f6d7ff73eca6bf83b5718a57ccb91b4
Vorobey <vorobyovvorobyov@gmail.com>
2023-11-25 12:39:35 +0300

    Add tests

---
+++ Assets/UGF.Assets.Editor.Tests/TestAssetCollectionIdFolderAsset.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using UGF.EditorTools.Runtime.Ids;
+using UnityEngine;
+
+namespace UGF.Assets.Editor.Tests
+{
+    [CreateAssetMenu(menuName = "Tests/TestAssetCollectionIdFolderAsset")]
+    public class TestAssetCollectionIdFolderAsset : AssetFolderIdCollectionAsset<TestAssetCollectionIdAsset, Material>
+    {
+        protected override IList<GlobalId> OnGetCollection()
+        {
+            return Collection.Assets;
+        }
+    }
+}
